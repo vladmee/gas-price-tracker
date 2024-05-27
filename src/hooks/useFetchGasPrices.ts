318aa0b45c0c5284cc9145ece14333b8f9470b29
vladmee <vladpresedinte@gmail.com>
2024-05-27 09:35:10 +0800

    Add data fetching

---
+++ src/hooks/useFetchGasPrices.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import useGasPrices, { type GasPrices } from "../store/useGasPrices";
+import { type GasPricesApiResponse } from "~/app/api/gas-prices/route";
+
+const fetchGasPrices = async (): Promise<GasPrices> => {
+  const response = await fetch("/api/gas-prices", {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("could not reach the api");
+  }
+  const data: GasPricesApiResponse = await response.json();
+  return data.result;
+};
+
+const useFetchGasPrices = () => {
+  const setData = useGasPrices((state) => state.setData);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["api-gas-prices"],
+    queryFn: fetchGasPrices,
+    gcTime: 0,
+    staleTime: 0,
+    refetchInterval: 10000, // fetch every 10 sec
+  });
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setData(data);
+    }
+  }, [isSuccess, data, setData]);
+};
+
+export default useFetchGasPrices;
